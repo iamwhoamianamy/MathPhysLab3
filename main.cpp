e81192a0a83f1cccfc6cf333cfca14e6d05e8ef2
krtisheva <Ksyushartisheva@mail.ru>
2021-05-05 13:45:35 +0700

    Отчет

--- main.cpp
+++ main.cpp
@@ -12,17 +12,17 @@ int main()
 
    ofstream fout("result.txt");
 
-   for (int p = 0; p < 3; p++)
-      for (int i = 0; i < 3; i++)
-         for (int j = 0; j < 3; j++)
-            for (int l = 0; l < 3; l++)
+   //for (int p = 0; p < 3; p++)
+      //for (int i = 0; i < 3; i++)
+         //for (int j = 0; j < 3; j++)
+            //for (int l = 0; l < 3; l++)
             {
                hp.InitializeMemory();
                hp.FormGlobalPortrait();
-               hp.test = Test(1, i, j, l, p);
+              // hp.test = Test(1, i, j, l, p);
+               hp.test = Test(1, 0, 2, 0, 2);
                hp.AssembleGlobalMatrix();
                hp.AccountFirstBound();
-               //fout << p << "\t" << i << "\t" << j << "\t" << l << "\t";
                hp.test.Print(fout);
                hp.Solve(fout);
                hp.PrintSolution(fout);
